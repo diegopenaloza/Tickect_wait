2f8651ed99d0cafcae86022ffc9b1a9db2724b55
Diego Peñaloza <isaacdiego25@gmail.com>
2023-01-04 12:21:46 -0500

    Scrap_code

---
+++ .ipynb_checkpoints/scrapi-checkpoint.py
@@ -0,0 +1,81 @@
+from playwright.sync_api import sync_playwright
+import time
+import time 
+from plyer import notification
+
+# +
+# direct="C:/Users/Diego/AppData/Local/Google/Chrome/User Data"
+
+direct="C:/Users/Diego/AppData/Local/Mozilla/Firefox/Profiles/ow35cx67.default"
+
+# +
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    browser = p.firefox.launch_persistent_context(channel="firefox"
+                                                                  ,headless=False,
+                                                                  user_data_dir=direct)
+    page = browser.new_page()
+    cedula="0102752763"
+    contraseña="mariana2023"
+    page.goto("https://app.iess.gob.ec/iess-gestion-agendamiento-citas-medicas-web/login/login.jsf")
+    time.sleep(3)
+    page.locator("input[data-p-rmsg='Ingrese su usuario.']").type(cedula, delay=100);
+    time.sleep(2)
+    page.locator("input[id='formLogin:keyPassword']").type(contraseña, delay=200);    
+    
+    time.sleep(3)
+    page.locator("button").click()
+    page.wait_for_load_state("networkidle")
+    time.sleep(3)
+    page.locator("button[id='formularioAgendamientoCitasMedicas:btnContinuar']").click()
+    # page.evaluate("""async () => { document.querySelector("button").click() }""")
+    page.wait_for_load_state("networkidle")
+    time.sleep(3)
+#     cdu=page.evaluate("""async () => {
+    
+#         var cedu=document.getElementsByTagName("td")[5].innerHTML
+        
+#         function sleep(ms) {
+#           return new Promise(resolve => setTimeout(resolve, ms));
+#         }
+
+#         async function delayedGreeting() {
+#           document.getElementById('formularioAgendamientoCitasMedicas:cmbProvincia_label').click()
+#           await sleep(1000);
+#           document.getElementById('formularioAgendamientoCitasMedicas:cmbProvincia_1').click()
+#           await sleep(1000);
+#           document.getElementById('formularioAgendamientoCitasMedicas:cmbCanton_label').click()
+#           await sleep(1000);
+#           document.getElementById('formularioAgendamientoCitasMedicas:cmbCanton_3').click()
+#           await sleep(1000);
+#           document.getElementById('formularioAgendamientoCitasMedicas:cmbArea_label').click()
+#           await sleep(1000);
+#           document.getElementById('formularioAgendamientoCitasMedicas:cmbArea_7').click()
+#           await sleep(1000);
+#           document.getElementById('formularioAgendamientoCitasMedicas:btnDisponibilidad').click()
+#         }
+#         delayedGreeting();
+#     return cedu
+#     }""")
+    time.sleep(30)
+    # print(cdu)
+    browser.close()
+
+# +
+# notification.notify(
+
+# title="Alerta",
+# message=" Existen Cosnsultas disponibles",
+#     # timeout=100
+# )
+
+# +
+# from plyer import notification
+
+# notification.notify(
+#     title='Título de la notificación',
+#     message='Mensaje de la notificación',
+#     app_name='Nombre de tu aplicación',
+#     timeout=10,  # Duración de la notificación en segundos
+#     # app_icon='sound.wav'  # Archivo de sonido a utilizar
+# )
